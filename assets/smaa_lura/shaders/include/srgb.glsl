047fc422aae3c230811d25768fc8081cf94d1c1c
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2026-07-13 23:48:46 +0200

    Revert "Checkpoint alternate sRGB conversion"
    This reverts commit 8d5c51802ba864eb84fa264fea4f3d7e9b01abe6.

--- assets/smaa_lura/shaders/include/srgb.glsl
+++ assets/smaa_lura/shaders/include/srgb.glsl
@@ -4,36 +4,30 @@
 	All rights reserved unless explicitly stated.
 */
 
-// Good approximations of sRGB <-> linear conversion,
-// utilizing the fact that the linear function is smaller/greater than the power function
-// almost exactly in the part of the input range where it should be used.
-//
-// See: https://www.desmos.com/calculator/fgh5rvycuo
-
 lowp float linear(lowp float srgb) {
-	return min(
-		pow(fma(srgb, 1.0/1.055, 0.055/1.055), 2.4),
-		srgb / 12.92
-	);
+	return (srgb > 0.04045)
+		? pow(fma(srgb, 1.0/1.055, 0.055/1.055), 2.4)
+		: (srgb / 12.92);
 }
 
 lowp vec3 linear(lowp vec3 srgb) {
-	return min(
-		pow(fma(srgb, vec3(1.0/1.055), vec3(0.055/1.055)), vec3(2.4)),
-		srgb / 12.92
+	return vec3(
+		linear(srgb.r),
+		linear(srgb.g),
+		linear(srgb.b)
 	);
 }
 
 lowp float srgb(lowp float linear) {
-	return max(
-		fma(pow(linear, 1.0/2.4), 1.055, -0.055),
-		linear * 12.92
-	);
+	return (linear > 0.0031308)
+		? fma(pow(linear, 1.0/2.4), 1.055, -0.055)
+		: (linear * 12.92);
 }
 
 lowp vec3 srgb(lowp vec3 linear) {
-	return max(
-		fma(pow(linear, vec3(1.0/2.4)), vec3(1.055), vec3(-0.055)),
-		linear * 12.92
+	return vec3(
+		srgb(linear.r),
+		srgb(linear.g),
+		srgb(linear.b)
 	);
 }
